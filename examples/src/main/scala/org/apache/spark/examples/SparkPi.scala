5a3eb3c87e90ad5ec0db1ce9ba955fbd4533f065
Kulshrestha, Kshitij <kshitij23@live.in>
2017-05-20 19:51:00 +0700

    Revert "Initial commit"
    This reverts commit e54a5c4adaba02cb91ebe654424b26a2a6dce17f.

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -25,16 +25,10 @@ import org.apache.spark.sql.SparkSession
 /** Computes an approximation to pi */
 object SparkPi {
   def main(args: Array[String]) {
-
-    System.setProperty("hive.metastore.uris", "thrift://localhost:9083")
-
     val spark = SparkSession
-      .builder()
-      .master("local[4]")
+      .builder
       .appName("Spark Pi")
-      .enableHiveSupport()
       .getOrCreate()
-
     val slices = if (args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
     val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
@@ -43,73 +37,7 @@ object SparkPi {
       if (x*x + y*y < 1) 1 else 0
     }.reduce(_ + _)
     println("Pi is roughly " + 4.0 * count / (n - 1))
-
-
-
-
-
-     val query1 =
-      """select Topic   from  one1 as table1 join   one2 as table2
-        |where  (table1.User_Name=table2.User_Name
-        |and  table1.`From`='From: 1.11913372.-2@multexinvestornetwork.com'
-        |and  table1.Subject="Subject: December 14, 2000 - Bear Stearns' predictions for"
-        |and  table1.Mime_Version='Mime-Version: 1.0'
-        |and table1.Content_Type="12345"
-        |and  table2.`Date`='Date: Mon, 9 Apr 2001 '
-        |and  table2.Message_ID='Message-ID: <12406231.1075848299564.JavaMail.evans@thyme>'
-        |and  table2.File_No='1.')
-        | or
-        | (table1.User_Name=table2.User_Name
-        |and  table1.`From`='From: jennifer.k.dickinson@usa.conoco.com'
-        |and  table1.Subject='Subject: hi'
-        |and  table1.Mime_Version='Mime-Version: 1.0'
-        |and table1.Content_Type="6789"
-        |and  table2.`Date`='Date: Mon, 9 Apr 2001 '
-        |and  table2.Message_ID='Message-ID: <12406231.1075848299564.JavaMail.evans@thyme>'
-        |and  table2.File_No='2.') """.stripMargin
-
-
-   /* val one= spark.sql( """select `From`,User_Name from one1 as table1
-                                |where  (table1.`From`='From: 1.11913372.-2@multexinvestornetwork.com'
-                                |and  table1.Subject="Subject: December 14, 2000 - Bear Stearns' predictions for"
-                                |and  table1.Mime_Version='Mime-Version: 1.0'
-                                |and table1.Content_Type="12345" )
-                                |or (table1.`From`='From: jennifer.k.dickinson@usa.conoco.com'
-                                |and  table1.Subject='Subject: hi'
-                                |and  table1.Mime_Version='Mime-Version: 1.0'
-                                |and table1.Content_Type="6789")""".stripMargin)
-
-
-
-    val two  = spark.sql("""select User_Name,Topic,File_No from  one2 as table2
-                                 |where  (table2.`Date`='Date: Mon, 9 Apr 2001 '
-                                 |and    table2.Message_ID='Message-ID: <12406231.1075848299564.JavaMail.evans@thyme>'
-                                 |and    (table2.File_No='1.' or table2.File_No='2.' ))""".stripMargin)
-
-    one.createOrReplaceTempView("table1filter")
-    two.createOrReplaceTempView("table2filter")
-
-    val finaltable ="""select Topic from table1filter , table2filter
-                      |where  (table1filter.User_Name=table2filter.User_Name
-                      |and table1filter.`From`='From: 1.11913372.-2@multexinvestornetwork.com'
-                      |and  table2filter.File_No='1.')
-                      |or (table1filter.User_Name=table2filter.User_Name
-                      |and  table1filter.`From`='From: jennifer.k.dickinson@usa.conoco.com'
-                      |and  table2filter.File_No='2.')""".stripMargin
-
-*/
-
-    val df=spark.sql(query1)
-
-    df.show(false)
-    Thread.sleep(40000)
     spark.stop()
   }
 }
-
-
-
-
-
-
-
+// scalastyle:on println
